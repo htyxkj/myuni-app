4d257fd6d9abc6acf9f7167c9ff81189456a4d74
LiZhengguo <lzg163email@163.com>
2021-02-04 16:33:08 +0800

    load修改

--- src/components/mLoad.vue
+++ src/components/mLoad.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="cu-load load-modal">
-		<image v-if="png" :src="png" mode="aspectFit"></image>
+		<image v-if="my_png" :src="my_png" mode="aspectFit"></image>
 		<view class="gray-text">{{msg?msg:'加载中...'}}</view>
 	</view>
 </template>
@@ -10,10 +10,19 @@
 	 * 加载页面通用类
 	 */
 	import {Vue,Prop,Component} from 'vue-property-decorator';
+	import comm from '@/static/js/comm.js';
 	@Component({})
 	export default class mLoad extends Vue {
 		@Prop({default:'',type:String}) msg?:string;
 		@Prop({default:'',type:String}) png?:string;
+		my_png:any = null;
+		onLoad(options:any) {
+			if(this.png){
+				this.my_png = this.png;
+			}else{
+				this.my_png = '/static/'+comm.ItemType+'.png';
+			}
+		}
 	}
 </script>
 
